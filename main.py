364dda57f51f752625af11bf9d4fc85542f591a3
Majdalenka420 <144223098+Majdalenka420@users.noreply.github.com>
2023-10-11 09:27:09 +0000

    program

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import requests
+import re
 
+x = requests.get('https://godzilla.fandom.com/wiki/Godzilla_(disambiguation)')
+headers = re.findall('h3.*?h3', x.text)
+for head in headers:
+    print("\n")
+    print(head)
