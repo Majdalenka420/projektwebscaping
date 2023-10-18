76e89540867eccb1b1c684c519625ebea9794e5a
Majdalenka420 <144223098+Majdalenka420@users.noreply.github.com>
2023-10-18 10:40:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import re
 x = requests.get('https://godzilla.fandom.com/wiki/Godzilla_(disambiguation)')
 headers = re.findall('h3.*?h3', x.text)
 for head in headers:
-    print("\n")
+    print("\n") 
     print(head)
